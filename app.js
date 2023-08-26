a9d86a0ec5fabe9f9fdce67cc3339207ae2409fd
patricksky006 <patrick.sky@hotmail.com>
2023-08-26 11:12:54 +0800

    implement updating restaurant

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express') //引入express模組
 const { engine } = require('express-handlebars') //引入express-handlebars的VIEW樣板引擎
+const methodOverride = require('method-override') //引入method-override的模組
 const app = express() // 建立了一個Express應用程式的實例，儲存在app常數中
 
 const port = 3000
@@ -7,13 +8,12 @@ const port = 3000
 const db = require('./models') //取得資料庫的model資料夾
 const restaurant = db.restaurant //取得model資料夾中restaurant檔案資料
 
-
-
 app.engine('.hbs', engine({extname: '.hbs'})) //告訴express使用express-handlebars樣版引擎，並指定附檔名為.hbs
 app.set('view engine', '.hbs') // 告訴express使用express-handlebars樣版引擎來處理render畫面
 app.set('views', './views') //視覺文件儲存在'./view'的目錄中
 app.use(express.static('public')) //載入靜態檔案，包含Bootstrap的CSS和JS
 app.use(express.urlencoded({ extended: true })) //使用此行來請求網址中獲取表單資料,否則就會回傳undefined的表單資料。
+app.use(methodOverride('_method')) //指定'_method'為query識別方法
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
@@ -43,7 +43,11 @@ app.get('/restaurants/:id', (req, res) => {
 
 app.get('/restaurants/:id/edit', (req,res) => {
   const id = req.params.id
-  res.send(`第${id}個restaurants的編輯頁`) //第${ID}個餐廳的編輯頁面
+  return restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'], //從資料庫中撈出要的表格欄位
+    raw: true //因在sequelize中的find功能會將結果轉換成model instances，而不是JavaScripts objects，所以sequelize有提供此參數藉以停用轉換。
+  })
+    .then((restaurant) => res.render('edit', { restaurant }))
 })
 
 app.post('/restaurants', (req, res) => {
@@ -62,7 +66,22 @@ app.post('/restaurants', (req, res) => {
 
 app.put('/restaurants/:id', (req, res) => {
   const id = req.params.id
-  res.send(`modified ${id} restaurant`) //更新餐廳
+  const body = req.body
+  
+  return restaurant.update({ 
+    name: body.name, 
+    name_en: body.name_en, 
+    category: body.category, 
+    image: body.image, 
+    location: body.location, 
+    phone: body.phone, 
+    google_map: body.google_map, 
+    rating: body.rating, 
+    description: body.description
+  }, {
+    where: { id }
+  })
+    .then(() => res.redirect(`/restaurants/${id}`))
 })
 
 app.delete('/restaurants:id', (req, res) => {
