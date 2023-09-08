2740d5326b60af6533160355046ad98b65ae3f96
patricksky006 <patrick.sky@hotmail.com>
2023-09-08 19:35:35 +0800

    implement register function

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const restaurants = require('./restaurants')
 const searchRouter = require('./search');
 
 const db = require('../models');
-const User = db.user
+const User = db.User
 
 router.use('/restaurants', restaurants)
 router.use('/search', searchRouter);
@@ -24,8 +24,40 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/users', (req, res) => {
-  res.send('POST/users, 新增使用者')
+router.post('/users', (req, res, next) => {
+  const { name, email, password, confirmedPassword } = req.body
+  if (!email || !password) {
+    req.flash('error', '信箱和密碼為必填欄位')
+    return res.redirect('back')
+  }
+
+  if (password !== confirmedPassword) {
+    req.flash('error', '輸入密碼與再次輸入密碼不相符')
+    return res.redirect('back')
+  }
+  
+  return User.count({ where: {email} })
+    .then((countEmail) => {
+      if (countEmail > 0) {
+        req.flash('error', '此信箱已經註冊過了')
+        return res.redirect('back')
+      }
+
+      return User.create({ name, email, password})
+        .then((createdUser) => {
+          if (!createdUser) {
+            req.flash('error', '資料庫有異常，註冊失敗')
+            return res.redirect('back')
+          }
+          req.flash('success', '註冊成功')
+          return res.redirect('/login')
+        })
+
+        .catch((error) => {
+          error.errorMessage = '不明原因，註冊失敗'
+          next(error)
+        })
+    })
 })
 
 router.post('/login', (req, res) => {
