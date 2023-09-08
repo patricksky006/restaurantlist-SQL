6c2a0239f2d6c0952d85ce7cb7fd8846f19be066
patricksky006 <patrick.sky@hotmail.com>
2023-09-08 15:19:16 +0800

    Add user:schema,model,router,page.hbs

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const router = express.Router()
 const restaurants = require('./restaurants')
 const searchRouter = require('./search');
 
+const db = require('../models');
+const User = db.user
+
 router.use('/restaurants', restaurants)
 router.use('/search', searchRouter);
 
@@ -13,5 +16,24 @@ router.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/users', (req, res) => {
+  res.send('POST/users, 新增使用者')
+})
+
+router.post('/login', (req, res) => {
+  res.send('POST/login, 登入使用者')
+})
+
+router.post('/logout', (req, res) => {
+  res.send('POST/logout, 登出使用者')
+})
 
 module.exports = router
