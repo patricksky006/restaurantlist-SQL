fdc02fb4a2b6fd986032dc01031564c003cfaea0
patricksky006 <patrick.sky@hotmail.com>
2023-09-09 20:51:07 +0800

    Add users router

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ const router = express.Router()
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 
+const restaurants = require('./restaurants')
+const searchRouter = require('./search')
+const usersRouter = require('./users')
+const authHandler = require('../middlewares/auth-handler');
+
+
+router.use('/restaurants',authHandler, restaurants)
+router.use('/search', searchRouter);
+router.use('/users', usersRouter);
+
 const db = require('../models');
 const User = db.User
 
@@ -36,13 +46,7 @@ passport.deserializeUser((foundUser, done) => { //第一個參數是取出登入
   done(null, { id: foundUser.id })
 })
 
-const restaurants = require('./restaurants')
-const searchRouter = require('./search');
-const authHandler = require('../middlewares/auth-handler');
 
-
-router.use('/restaurants',authHandler, restaurants)
-router.use('/search', searchRouter);
 
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
@@ -56,41 +60,7 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/users', (req, res, next) => {
-  const { name, email, password, confirmedPassword } = req.body
-  if (!email || !password) {
-    req.flash('error', '信箱和密碼為必填欄位')
-    return res.redirect('back')
-  }
 
-  if (password !== confirmedPassword) {
-    req.flash('error', '輸入密碼與再次輸入密碼不相符')
-    return res.redirect('back')
-  }
-  
-  return User.count({ where: {email} })
-    .then((countEmail) => {
-      if (countEmail > 0) {
-        req.flash('error', '此信箱已經註冊過了')
-        return res.redirect('back')
-      }
-
-      return User.create({ name, email, password})
-        .then((createdUser) => {
-          if (!createdUser) {
-            req.flash('error', '資料庫有異常，註冊失敗')
-            return res.redirect('back')
-          }
-          req.flash('success', '註冊成功')
-          return res.redirect('/login')
-        })
-
-        .catch((error) => {
-          error.errorMessage = '不明原因，註冊失敗'
-          next(error)
-        })
-    })
-})
 
 router.post('/login', passport.authenticate('local', {  //第一個參數式Strategy名稱(local), 第二個是自訂的選項
   successRedirect: '/restaurants',
---
+++ routes/users.js
@@ -0,0 +1,45 @@
+//使用者帳號密碼路由模組
+
+const express = require('express')
+const router = express.Router()
+
+const db = require('../models');
+const User = db.User
+
+router.post('/', (req, res, next) => {
+  const { name, email, password, confirmedPassword } = req.body
+  if (!email || !password) {
+    req.flash('error', '信箱和密碼為必填欄位')
+    return res.redirect('back')
+  }
+
+  if (password !== confirmedPassword) {
+    req.flash('error', '輸入密碼與再次輸入密碼不相符')
+    return res.redirect('back')
+  }
+  
+  return User.count({ where: {email} })
+    .then((countEmail) => {
+      if (countEmail > 0) {
+        req.flash('error', '此信箱已經註冊過了')
+        return res.redirect('back')
+      }
+
+      return User.create({ name, email, password})
+        .then((createdUser) => {
+          if (!createdUser) {
+            req.flash('error', '資料庫有異常，註冊失敗')
+            return res.redirect('back')
+          }
+          req.flash('success', '註冊成功')
+          return res.redirect('/login')
+        })
+
+        .catch((error) => {
+          error.errorMessage = '不明原因，註冊失敗'
+          next(error)
+        })
+    })
+})
+
+module.exports = router
