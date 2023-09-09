93d97409ec832195d852d0d1ba1dea36d60be1e4
patricksky006 <patrick.sky@hotmail.com>
2023-09-09 22:16:01 +0800

    Refresh seed information

--- seeders/20230825061124-initial-data.js
+++ seeders/20230825061124-initial-data.js
@@ -4,24 +4,58 @@
 
 const fs = require('fs')
 const path = require('path')
+const bcrypt = require('bcryptjs')
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const jsonFilePath = path.resolve(__dirname, '../public/jsons/restaurant.json') // 找到JSON檔案位置
-    const jsonData = fs.readFileSync(jsonFilePath, 'utf8') // 同步讀取文件內容
-    const restaurants = JSON.parse(jsonData).results // 將文件內容中results下的內容取出來
+  async up(queryInterface, Sequelize) {
+    let transaction
+    try {
+      // 为了避免處理資料失誤，使用 transaction
+      transaction = await queryInterface.sequelize.transaction()
 
-    const now = new Date() // 因JSON文件中沒有createdAt和updatedAt兩個欄位，所以要用map方法加入這兩種屬性
-    const dataWithTimestamps = restaurants.map((restaurant) => ({
-      ...restaurant,
-      createdAt: now,
-      updatedAt: now
-    }))
-
-    await queryInterface.bulkInsert('Restaurants', dataWithTimestamps)
+      // 新增使用者種子資料
+      const hash = await bcrypt.hash('12345678', 10)
+      await queryInterface.bulkInsert('Users', [
+        {
+          id: 1,
+          name: 'user1',
+          email: 'user1@example.com',
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: 'user2',
+          email: 'user2@example.com',
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ], { transaction })
+      // 新增資料庫種子資料
+      const jsonFilePath = path.resolve(__dirname, '../public/jsons/restaurant.json') // 找到 JSON 檔案位置
+      const jsonData = fs.readFileSync(jsonFilePath, 'utf8') // 同步讀取文件內容
+      const restaurants = JSON.parse(jsonData).results // 將文件內容中 results 下的內容取出來
+      const restaurantUserMapping = {
+        1: [1, 2, 3, 7],
+        2: [4, 5, 6, 8],
+      }
+      const dataWithTimestamps = restaurants.map((restaurant, index) => ({
+        ...restaurant,
+        userId: restaurantUserMapping[1].includes(index + 1) ? 1 : 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
+      await queryInterface.bulkInsert('Restaurants', dataWithTimestamps, { transaction })
+      await transaction.commit()
+    } catch (error) {
+      if (transaction) transaction.rollback()
+    }
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null)
     await queryInterface.bulkDelete('Restaurants', null)
-  }
+  },
 }
