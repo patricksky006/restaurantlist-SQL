e33d4d80c500fbe25b6dca6a45b736155ff326ed
patricksky006 <patrick.sky@hotmail.com>
2023-09-09 20:28:44 +0800

    implement auth check in todos features

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -7,10 +7,10 @@ const db = require('../models')
 const restaurant = db.restaurant 
 
 router.get('/', (req, res, next) => {
-  console.log(req.user)
   const page = parseInt(req.query.page) || 1
 	const limit = 6
   const sort = req.query.sort 
+  const userId = req.user.id //此id是由passport套件提供
 
   const sortOptions = {
     name_asc: [['name', 'ASC']],
@@ -21,6 +21,7 @@ router.get('/', (req, res, next) => {
 
   restaurant.findAll({
     attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'],
+    where: { userId },
     offset: (page - 1) * limit,
     limit,
     order: sortOptions[sort],
@@ -47,11 +48,24 @@ router.get('/new', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id
+  const userId = req.user.id //此id是由passport套件提供
   return restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'], 
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description', 'userId'], 
     raw: true // 因在sequelize中的find功能會將結果轉換成model instances，而不是JavaScripts objects，所以sequelize有提供此參數藉以停用轉換。
   })
-    .then((restaurant) => res.render('detail', { restaurant }))
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '資料不存在')
+        return res.redirect('/restaurants')
+      }
+
+      if ( userId !== restaurant.userId ) {
+        req.flash('error', '您沒有操作的權限')
+        return res.redirect('/restaurants')
+      }
+
+      res.render('detail', { restaurant })
+  })
     .catch((error) => {
       error.errorMessage = '資料取得失敗:('
 			next(error)
@@ -60,11 +74,24 @@ router.get('/:id', (req, res, next) => {
 
 router.get('/:id/edit', (req, res, next) => {
   const id = req.params.id
+  const userId = req.user.id //此id是由passport套件提供
   return restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description'], 
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description', 'userId'], 
     raw: true 
   })
-    .then((restaurant) => res.render('edit', { restaurant}))
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '資料不存在')
+        return res.redirect('/restaurants')
+      }
+
+      if ( userId !== restaurant.userId ) {
+        req.flash('error', '您沒有操作的權限')
+        return res.redirect('/restaurants')
+      }
+
+      res.render('edit', { restaurant})
+  })
     .catch((error) => {
       error.errorMessage = '資料取得失敗:('
 			next(error)
@@ -73,7 +100,9 @@ router.get('/:id/edit', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
     const { name, name_en, category, image, location, phone, google_map, rating, description} = req.body
-    return restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    const userId = req.user.id //此id是由passport套件提供
+
+    return restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
       .then(() => {
         req.flash('success', '餐廳已新增') // 進行新增動作時，透過flash存入ket-value值，並在/restaurants取出使用
         return res.redirect('/restaurants')
@@ -87,14 +116,28 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
     const id = req.params.id
     const { name, name_en, category, image, location, phone, google_map, rating, description} = req.body
+    const userId = req.user.id //此id是由passport套件提供
 
-    return restaurant.update({name, name_en, category, image, location, phone, google_map, rating, description}, {
-    where: { id }
+    return restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description', 'userId']
   })
-    .then(() => {
-      req.flash('success', '內容已更新')
-      res.redirect(`/restaurants/${id}`)
-  })
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '資料不存在')
+        return res.redirect('/restaurants')
+      }
+
+      if ( userId !== restaurant.userId ) {
+        req.flash('error', '您沒有操作的權限')
+        return res.redirect('/restaurants')
+      }
+
+      return restaurant.update({name, name_en, category, image, location, phone, google_map, rating, description})
+        .then(() => {
+          req.flash('success', '內容已更新')
+          res.redirect(`/restaurants/${id}`)
+        })
+    })
     .catch((error) => {
       error.errorMessage = '更新失敗'
       next(error)
@@ -102,17 +145,37 @@ router.put('/:id', (req, res, next) => {
 
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     const id = req.params.id
-    return restaurant.destroy({ where: { id } })
-      .then(() => {
-        req.flash('success', '餐廳已刪除')
-        res.redirect('/restaurants')
+    const userId = req.user.id //此id是由passport套件提供
+
+    return restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'rating', 'description', 'userId'], 
+    raw: true // 因在sequelize中的find功能會將結果轉換成model instances，而不是JavaScripts objects，所以sequelize有提供此參數藉以停用轉換。
+    })
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '資料不存在')
+        return res.redirect('/restaurants')
+      }
+
+      if ( userId !== restaurant.userId ) {
+        req.flash('error', '您沒有操作的權限')
+        return res.redirect('/restaurants')
+      }
+      return
+    })
+    .then(() => {
+      return restaurant.destroy({ where: { id } })
+        .then(() => {
+          req.flash('success', '餐廳已刪除')
+          res.redirect('/restaurants')
+      })
     })
       .catch((error) => {
       error.errorMessage = '刪除失敗'
       next(error)
-    })
+       })
 })
 
 module.exports = router
